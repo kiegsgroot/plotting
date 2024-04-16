f0da10f0899327ddcfff3a48fad4141995eb78f2
kiegsgroot <kiegan@the-groots.ca>
2024-04-16 19:52:53 +0000

    removed unneeded endpoint

--- plotting/themes/themev2.py
+++ plotting/themes/themev2.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
 import importlib.resources as pkg_resources
 from ..common.functions.load_yaml import load_yaml_as_dict
-import contextlib
 
 class Theme(BaseModel):
     theme_name: str
