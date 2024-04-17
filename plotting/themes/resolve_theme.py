9a5c8f411d453eb30af686d6237abd41458de47f
kiegsgroot <kiegan@the-groots.ca>
2024-04-17 17:30:07 +0000

    corrected parent class to BaseTheme

--- plotting/themes/resolve_theme.py
+++ plotting/themes/resolve_theme.py
@@ -1,9 +1,8 @@
 from pydantic import BaseModel
-import importlib.resources as pkg_resources
-from ..common.functions.load_yaml import load_yaml_as_dict
 from typing import Literal
+from .base_theme import BaseTheme
 
-class ReSolveTheme(BaseModel):
+class ReSolveTheme(BaseTheme):
     theme_name: Literal['ReSolve'] = 'ReSolve'
     colors: list[str] = [
     "#00488d", "#d3d800", "#f29400", "#6f4596", "#89d2ff", 
--- plotting/themes/return_stacked_theme.py
+++ plotting/themes/return_stacked_theme.py
@@ -1,9 +1,8 @@
 from pydantic import BaseModel
-import importlib.resources as pkg_resources
-from ..common.functions.load_yaml import load_yaml_as_dict
 from typing import Literal
+from .base_theme import BaseTheme
 
-class ReturnStackedTheme(BaseModel):
+class ReturnStackedTheme(BaseTheme):
     theme_name: Literal['Return-Stacked'] = 'Return-Stacked'
     colors: list[str] = ['#323A46', '#3A6A9C', '#95E885', '#008EB8', '#14CFA6', '#EBE96A']
 
