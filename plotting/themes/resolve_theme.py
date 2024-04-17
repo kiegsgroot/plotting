fed901413c27028b487e568deb2919b0d69544b0
kiegsgroot <kiegan@the-groots.ca>
2024-04-17 22:27:34 +0000

    reformated

--- plotting/themes/resolve_theme.py
+++ plotting/themes/resolve_theme.py
@@ -5,9 +5,9 @@ from .base_theme import BaseTheme
 class ReSolveTheme(BaseTheme):
     theme_name: Literal['ReSolve'] = 'ReSolve'
     colors: list[str] = [
-    "#00488d", "#d3d800", "#f29400", "#6f4596", "#89d2ff", 
-    "#b3d852", "#7e768c", "#81aec6", "#e6c245", "#63cc77", 
-    "#00abff", "#293645", "#fbba00", "#88949e"
+        "#00488d", "#d3d800", "#f29400", "#6f4596", "#89d2ff", 
+        "#b3d852", "#7e768c", "#81aec6", "#e6c245", "#63cc77", 
+        "#00abff", "#293645", "#fbba00", "#88949e"
     ]
 
 
--- plotting/themes/return_stacked_theme.py
+++ plotting/themes/return_stacked_theme.py
@@ -4,5 +4,7 @@ from .base_theme import BaseTheme
 
 class ReturnStackedTheme(BaseTheme):
     theme_name: Literal['Return-Stacked'] = 'Return-Stacked'
-    colors: list[str] = ['#323A46', '#3A6A9C', '#95E885', '#008EB8', '#14CFA6', '#EBE96A']
+    colors: list[str] = [
+        '#323A46', '#3A6A9C', '#95E885', '#008EB8', '#14CFA6', '#EBE96A'
+        ]
 
