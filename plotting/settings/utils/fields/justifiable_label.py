6288139c41e2a6ffe34853ca96e935dadfb9eda1
kiegsgroot <kiegan@the-groots.ca>
2024-04-17 23:12:52 +0000

    updated literals

--- plotting/settings/utils/fields/justifiable_label.py
+++ plotting/settings/utils/fields/justifiable_label.py
@@ -3,7 +3,7 @@ from typing import Literal
 from .label import Label
 
 class JustifiableLabel(Label):
-    justify: Literal["0", "0.5", "1"] = Field(
-        default="0",
+    justify: Literal[0, 0.5, 1] = Field(
+        default=0,
         title="Justify",
     )
