d4dd31ffaa3172ddbd1b80621b6064f57eafd850
kiegsgroot <kiegan@the-groots.ca>
2024-04-18 18:44:07 +0000

    set default dates

--- plotting/settings/utils/groups/date_settings.py
+++ plotting/settings/utils/groups/date_settings.py
@@ -1,19 +1,16 @@
 from pydantic import Field, constr
 from .base_settings_group import BaseSettingsGroup
 from datetime import datetime
-
 class DateSettings(BaseSettingsGroup):
     start_date: str = Field(
-        ...,
+        default="1980-01-01",
         title="Start Date", 
         pattern=r'^\d{4}-\d{2}-\d{2}$',
-        examples=['2000-01-01'],
     )
     end_date: str = Field(
-        ...,
+        default=datetime.now().strftime("%Y-%m-%d"),
         title="End Date", 
         pattern=r'^\d{4}-\d{2}-\d{2}$', 
-        examples=['2010-01-01'], 
     )
 
     def apply(self, plot):
